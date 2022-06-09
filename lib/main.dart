2302519840310e014b3a069680c0ce481db13688
tiago <tiago.moreira@timebrisa.com.br>
2022-06-09 17:52:32 -0300

    feat:

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:navigation_drawer_example/widget/button_widget.dart';
 import 'package:navigation_drawer_example/widget/navigation_drawer_widget.dart';
 
 Future main() async {
@@ -42,14 +41,6 @@ class _MainPageState extends State<MainPage> {
           builder: (context) => Container(
             alignment: Alignment.center,
             padding: EdgeInsets.symmetric(horizontal: 32),
-            child: ButtonWidget(
-              icon: Icons.open_in_new,
-              text: 'Open Drawer',
-              onClicked: () {
-                Scaffold.of(context).openDrawer();
-                // Scaffold.of(context).openEndDrawer();
-              },
-            ),
           ),
         ),
       );
--- lib/widget/navigation_drawer_widget.dart
+++ lib/widget/navigation_drawer_widget.dart
@@ -3,79 +3,109 @@ import 'package:navigation_drawer_example/page/favourites_page.dart';
 import 'package:navigation_drawer_example/page/people_page.dart';
 import 'package:navigation_drawer_example/page/user_page.dart';
 
+import '../shared/themes/app_text_styles.dart';
+
 class NavigationDrawerWidget extends StatelessWidget {
   final padding = EdgeInsets.symmetric(horizontal: 20);
   @override
   Widget build(BuildContext context) {
-    final name = 'Sarah Abs';
-    final email = 'sarah@abs.com';
-    final urlImage =
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80';
+    final name = 'Osvaldo Crispim';
+    final email = '(Osvaldo Crispim)';
+    final urlImage = '../../assets/perfil.jpeg';
 
     return Drawer(
-      child: Material(
-        color: Color.fromRGBO(50, 75, 205, 1),
-        child: ListView(
-          children: <Widget>[
-            buildHeader(
-              urlImage: urlImage,
-              name: name,
-              email: email,
-              onClicked: () => Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => UserPage(
-                  name: 'Sarah Abs',
-                  urlImage: urlImage,
-                ),
-              )),
-            ),
-            Container(
-              padding: padding,
-              child: Column(
-                children: [
-                  const SizedBox(height: 12),
-                  buildSearchField(),
-                  const SizedBox(height: 24),
-                  buildMenuItem(
-                    text: 'People',
-                    icon: Icons.people,
-                    onClicked: () => selectedItem(context, 0),
+      child: Container(
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("../assets/sidebar.jpg"),
+                  fit: BoxFit.cover)),
+          child: ListView(
+            children: <Widget>[
+              buildHeader(
+                urlImage: urlImage,
+                name: name,
+                email: email,
+                onClicked: () => Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => UserPage(
+                    name: 'Sarah Abs',
+                    urlImage: urlImage,
                   ),
-                  const SizedBox(height: 16),
-                  buildMenuItem(
-                    text: 'Favourites',
-                    icon: Icons.favorite_border,
-                    onClicked: () => selectedItem(context, 1),
-                  ),
-                  const SizedBox(height: 16),
-                  buildMenuItem(
-                    text: 'Workflow',
-                    icon: Icons.workspaces_outline,
-                    onClicked: () => selectedItem(context, 2),
-                  ),
-                  const SizedBox(height: 16),
-                  buildMenuItem(
-                    text: 'Updates',
-                    icon: Icons.update,
-                    onClicked: () => selectedItem(context, 3),
-                  ),
-                  const SizedBox(height: 24),
-                  Divider(color: Colors.white70),
-                  const SizedBox(height: 24),
-                  buildMenuItem(
-                    text: 'Plugins',
-                    icon: Icons.account_tree_outlined,
-                    onClicked: () => selectedItem(context, 4),
-                  ),
-                  const SizedBox(height: 16),
-                  buildMenuItem(
-                    text: 'Notifications',
-                    icon: Icons.notifications_outlined,
-                    onClicked: () => selectedItem(context, 5),
-                  ),
-                ],
+                )),
               ),
-            ),
-          ],
+              Container(
+                padding: padding,
+                child: Column(
+                  children: [
+                    // buildSearchField(),
+                    Divider(color: Colors.white70),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Pessoal',
+                        style: TextStyles.titleRegular,
+                      ),
+                    ),
+                    buildMenuItem(
+                      text: 'Organograma',
+                      icon: Icons.view_module_sharp,
+                      onClicked: () => selectedItem(context, 0),
+                    ),
+                    const SizedBox(height: 16),
+                    buildMenuItem(
+                      text: 'Formar Organograma',
+                      icon: Icons.control_point_duplicate_outlined,
+                      onClicked: () => selectedItem(context, 1),
+                    ),
+                    const SizedBox(height: 16),
+                    buildMenuItem(
+                      text: 'HomeOffice/Covid',
+                      icon: Icons.account_circle_rounded,
+                      onClicked: () => selectedItem(context, 2),
+                    ),
+                    const SizedBox(height: 16),
+                    buildMenuItem(
+                      text: 'Meus Subordinados',
+                      icon: Icons.supervisor_account_outlined,
+                      onClicked: () => selectedItem(context, 3),
+                    ),
+                    const SizedBox(height: 16),
+                    buildMenuItem(
+                      text: 'Minhas Ocorrências',
+                      icon: Icons.file_copy,
+                      onClicked: () => selectedItem(context, 4),
+                    ),
+                    const SizedBox(height: 16),
+                    buildMenuItem(
+                      text: 'Mensagens',
+                      icon: Icons.mail,
+                      onClicked: () => selectedItem(context, 5),
+                    ),
+                     Divider(color: Colors.white70),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Relatórios',
+                        style: TextStyles.titleRegular,
+                      ),
+                    ),
+                 
+                    buildMenuItem(
+                      text: 'Controle do Efetivo',
+                      icon: Icons.content_paste_search_outlined,
+                      onClicked: () => selectedItem(context, 33),
+                    ),
+                    const SizedBox(height: 16),
+                    buildMenuItem(
+                      text: 'Mapa de Vagas',
+                      icon: Icons.map_sharp,
+                      onClicked: () => selectedItem(context, 33),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -109,40 +139,34 @@ class NavigationDrawerWidget extends StatelessWidget {
                   ),
                 ],
               ),
-              Spacer(),
-              CircleAvatar(
-                radius: 24,
-                backgroundColor: Color.fromRGBO(30, 60, 168, 1),
-                child: Icon(Icons.add_comment_outlined, color: Colors.white),
-              )
             ],
           ),
         ),
       );
 
-  Widget buildSearchField() {
-    final color = Colors.white;
+  // Widget buildSearchField() {
+  //   final color = Colors.white;
 
-    return TextField(
-      style: TextStyle(color: color),
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        hintText: 'Search',
-        hintStyle: TextStyle(color: color),
-        prefixIcon: Icon(Icons.search, color: color),
-        filled: true,
-        fillColor: Colors.white12,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: BorderSide(color: color.withOpacity(0.7)),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: BorderSide(color: color.withOpacity(0.7)),
-        ),
-      ),
-    );
-  }
+  //   return TextField(
+  //     style: TextStyle(color: color),
+  //     decoration: InputDecoration(
+  //       contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+  //       hintText: 'Search',
+  //       hintStyle: TextStyle(color: color),
+  //       prefixIcon: Icon(Icons.search, color: color),
+  //       filled: true,
+  //       fillColor: Colors.white12,
+  //       enabledBorder: OutlineInputBorder(
+  //         borderRadius: BorderRadius.circular(5),
+  //         borderSide: BorderSide(color: color.withOpacity(0.7)),
+  //       ),
+  //       focusedBorder: OutlineInputBorder(
+  //         borderRadius: BorderRadius.circular(5),
+  //         borderSide: BorderSide(color: color.withOpacity(0.7)),
+  //       ),
+  //     ),
+  //   );
+  // }
 
   Widget buildMenuItem({
     required String text,
